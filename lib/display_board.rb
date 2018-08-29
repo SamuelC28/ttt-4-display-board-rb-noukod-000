c5b401a3a69f302c7c92fe313778cbc2d351d312
Samuel Cornet <samuel@noukod.com>
2018-08-29 16:45:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
+def display_board
+  separated_cells  = "   |   |   \n"
+  separated_col = "-----------\n"
+  puts separated_cells
+  puts separated_col
+  puts separated_cells
+  puts separated_col
+  puts separated_cells
+end
+display_board
 # Define display_board that accepts a board and prints
 # out the current state.
