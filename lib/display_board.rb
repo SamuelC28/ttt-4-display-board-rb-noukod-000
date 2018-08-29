612e5d68e8a7d698c33b1eb2df6d435a342e529a
Samuel Cornet <samuel@noukod.com>
2018-08-29 17:25:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
   #board = [" "," "," "," "," "," "," "," "," "]
   puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
-  separated_col = "-----------\n"
+  puts ("-----------")
   puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
-  separated_col = "-----------\n"
+  puts ("-----------")
   puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
 end
 
